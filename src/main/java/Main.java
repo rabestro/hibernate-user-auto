808384f1210ae91c8711e868e76a702349152c90
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-08-31 23:01:01 +0300

    Update configuration

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,40 @@
 import models.Auto;
 import models.User;
 import services.UserService;
+import ui.Menu;
 
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
+        final var sc = new Scanner(System.in);
+        final var us = new UserService();
 
-        UserService userService = new UserService();
-//        User user = userService.findUser(2);
-//        System.out.println(user);
-//        System.out.println(user.getAutos());
+//        new Menu("Simple user management system")
+//                .add("List of all users", () -> us.findAllUsers().forEach(System.out::println))
+//                .add("Find user by id", () -> System.out.println(us.findUser(sc.nextInt())))
+//                .run();
 
-        User user = new User("Петя",42);
-        userService.saveUser(user);
 
-        Auto honda = new Auto("Honda Edix", "blue");
-        honda.setUser(user);
-        user.addAuto(honda);
+        User user = us.findUser(2);
+        System.out.println(user);
+        System.out.println(user.getAutos());
 
-        Auto ford = new Auto("Audi", "silver");
-        ford.setUser(user);
-        user.addAuto(ford);
-        userService.updateUser(user);
+//        User user = new User("Петя", 42);
+//        us.saveUser(user);
+//
+//        Auto honda = new Auto("Honda Edix", "blue");
+//        honda.setUser(user);
+//        user.addAuto(honda);
+//
+//        Auto ford = new Auto("Audi", "silver");
+//        ford.setUser(user);
+//        user.addAuto(ford);
+//        us.updateUser(user);
+    }
+
+    static void findUser() {
+
     }
 }
