da7c05d2d6476ca95b326b91392ebb844b278e7e
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-08-31 22:16:02 +0300

    Update configuration

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,20 +8,20 @@ public class Main {
     public static void main(String[] args) throws SQLException {
 
         UserService userService = new UserService();
-        User user = userService.findUser(2);
-        System.out.println(user);
-        System.out.println(user.getAutos());
+//        User user = userService.findUser(2);
+//        System.out.println(user);
+//        System.out.println(user.getAutos());
 
-//        User user = new User("Петя",42);
-//        userService.saveUser(user);
-//
-//        Auto honda = new Auto("Honda Edix", "blue");
-//        honda.setUser(user);
-//        user.addAuto(honda);
-//
-//        Auto ford = new Auto("Audi", "silver");
-//        ford.setUser(user);
-//        user.addAuto(ford);
-//        userService.updateUser(user);
+        User user = new User("Петя",42);
+        userService.saveUser(user);
+
+        Auto honda = new Auto("Honda Edix", "blue");
+        honda.setUser(user);
+        user.addAuto(honda);
+
+        Auto ford = new Auto("Audi", "silver");
+        ford.setUser(user);
+        user.addAuto(ford);
+        userService.updateUser(user);
     }
 }
