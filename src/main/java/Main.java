01cafc2246141b24e81ca28ffce8d28af172b954
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-08-31 19:55:36 +0300

    Update configuration

---
+++ src/main/java/Main.java
@@ -0,0 +1,26 @@
+import models.Auto;
+import models.User;
+import services.UserService;
+
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+
+        UserService userService = new UserService();
+        User user = userService.findUser(2);
+        System.out.println(user);
+
+//        User user = new User("Петя",42);
+//        userService.saveUser(user);
+//
+//        Auto honda = new Auto("Honda Edix", "blue");
+//        honda.setUser(user);
+//        user.addAuto(honda);
+//
+//        Auto ford = new Auto("Audi", "silver");
+//        ford.setUser(user);
+//        user.addAuto(ford);
+//        userService.updateUser(user);
+    }
+}
