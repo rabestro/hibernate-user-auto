e50d361bcd73ae3f4ca85aad8a6a7e3fc0e82eab
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-09-01 11:26:13 +0300

    Added menu

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,40 +1,8 @@
-import models.Auto;
-import models.User;
 import services.UserService;
-import ui.Menu;
-
-import java.sql.SQLException;
-import java.util.Scanner;
+import ui.Application;
 
 public class Main {
-    public static void main(String[] args) throws SQLException {
-        final var sc = new Scanner(System.in);
-        final var us = new UserService();
-
-//        new Menu("Simple user management system")
-//                .add("List of all users", () -> us.findAllUsers().forEach(System.out::println))
-//                .add("Find user by id", () -> System.out.println(us.findUser(sc.nextInt())))
-//                .run();
-
-
-        User user = us.findUser(2);
-        System.out.println(user);
-        System.out.println(user.getAutos());
-
-//        User user = new User("Петя", 42);
-//        us.saveUser(user);
-//
-//        Auto honda = new Auto("Honda Edix", "blue");
-//        honda.setUser(user);
-//        user.addAuto(honda);
-//
-//        Auto ford = new Auto("Audi", "silver");
-//        ford.setUser(user);
-//        user.addAuto(ford);
-//        us.updateUser(user);
-    }
-
-    static void findUser() {
-
+    public static void main(String[] args) {
+        new Application(new UserService()).run();
     }
 }
---
+++ src/main/java/ui/Application.java
@@ -0,0 +1,46 @@
+package ui;
+
+import models.User;
+import services.UserService;
+
+import java.util.Scanner;
+
+public class Application implements Runnable {
+    final static Scanner sc = new Scanner(System.in);
+    private final UserService service;
+
+    public Application(UserService userService) {
+        this.service = userService;
+
+    }
+
+    public void run() {
+        new Menu("Simple user management system")
+                .add("List of all users", () -> service.findAllUsers().forEach(System.out::println))
+                .add("Find user by id", this::findUser)
+                .add("Add new user", this::addUser)
+                .add("Delete user", this::deleteUser)
+                .run();
+    }
+
+    void addUser() {
+        System.out.println("Enter user name:");
+        var name = sc.nextLine();
+        System.out.println("Enter user age:");
+        var age = sc.nextInt();
+        service.saveUser(new User(name, age));
+    }
+
+    void deleteUser() {
+        System.out.println("Enter user id:");
+        var id = Integer.parseInt(sc.nextLine());
+        service.deleteUser(service.findUser(id));
+    }
+
+    void findUser() {
+        System.out.println("Enter user id:");
+        var id = Integer.parseInt(sc.nextLine());
+        var user = service.findUser(id);
+        System.out.println(user);
+    }
+}
